4394a26f082e0952777e2b33cfb4b5c398de8777
Yacine OUCHENE <youchene.prestataire@si2m.fr>
2019-01-02 16:49:51 +0100

    ajouter le module de routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app.routing.module';
 import { AppComponent } from './app.component';
 import { ProduitComponent } from './produit/produit.component';
 import { ProduitMockService } from './produit/produit.mock.service';
 import { Produit } from './shared/produit';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
-import { ContentComponent } from './content/content.component'
+import { ContentComponent } from './content/content.component';
+import { DashboardComponent } from './dashboard/dashboard.component'
  
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { ContentComponent } from './content/content.component'
     ProduitComponent,
     NavbarComponent,
     SidebarComponent,
-    ContentComponent
+    ContentComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
